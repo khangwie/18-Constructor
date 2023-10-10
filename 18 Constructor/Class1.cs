db8f242bd4fa4b1f388f4399c142bd46ca1d1bee
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-10 21:17:39 +0200

    Add project files.

---
+++ 18 Constructor/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Constructor
+{
+     internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+
+        }
+    }
+}
---
+++ 18 Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Constructor
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry porter", "JK Rowling", 400);
+
+            Console.WriteLine(book1.title);
+            Console.ReadLine();
+        }
+    }
+}
